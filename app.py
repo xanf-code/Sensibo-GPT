74cfc71c6491de99802d11e1134d1b471d08dd37
xanf-code <darshanaswathh@gmail.com>
2023-04-21 22:42:11 +0530

    moved to lambda/Gateway

--- app.py
+++ app.py
@@ -1,25 +1,42 @@
-from flask import Flask, jsonify
-from sensibo import main
+from sensibo import main, ac_details
 from cron_ac_params import dynamic_ac_ai
+from settings import MODE
+import json
 
-app = Flask(__name__)
+if MODE == "dev":
+    from flask import Flask, jsonify
 
+    app = Flask(__name__)
 
-@app.route('/ac', methods=['POST'])
-def ac_toggle():
-    main()
-    response = jsonify({'message': '[INFO] AC toggle run.'})
-    response.status_code = 200
-    return response
+    @app.route('/ac', methods=['POST'])
+    def ac_toggle():
+        main()
+        response = jsonify({'message': '[INFO] AC toggle run.'})
+        response.status_code = 200
+        return response
 
+    @app.route('/dynamic_ac', methods=['POST'])
+    def dynamic_params():
+        dynamic_ac_ai()
+        response = jsonify({'message': '[INFO] Dynamic AC run.'})
+        response.status_code = 200
+        return response
 
-@app.route('/dynamic_ac', methods=['POST'])
-def dynamic_params():
-    dynamic_ac_ai()
-    response = jsonify({'message': '[INFO] Dynamic AC run.'})
-    response.status_code = 200
-    return response
+    if __name__ == '__main__':
+        app.run()
 
-
-if __name__ == '__main__':
-    app.run()
+elif MODE == "prod":
+    def lambda_handler(event, context):
+        if event['rawPath'] == "/ac":
+            main()
+            return {"message": "[INFO] AC toggle run."}
+        elif event['rawPath'] == "/dynamic_ac":
+            dynamic_ac_ai()
+            global_json = ac_details()
+            ac_status = global_json["sensibo_data"][0]["ac_state"]
+            if ac_status:
+                return {"message": "[INFO] Dynamic AC run."}
+            else:
+                return {"message": "[INFO] AC turned off."}
+        else:
+            return {"message": "Invalid Endpoint"}
--- cron_ac_params.py
+++ cron_ac_params.py
@@ -1,11 +1,10 @@
-from sensibo import get_ac_state, lowest_highest_ai, ac_details, set_ac_temp, is_debug_mode, test_gpt_op
-
-ac_status = get_ac_state()
+from sensibo import lowest_highest_ai, ac_details, set_ac_temp, is_debug_mode, test_gpt_op
 
 
 def dynamic_ac_ai():
+    global_json = ac_details()
+    ac_status = global_json["sensibo_data"][0]["ac_state"]
     if ac_status:
-        global_json = ac_details()
         if is_debug_mode:
             print("DEBUG: TEST MODE")
             range = test_gpt_op["Temperature"]
