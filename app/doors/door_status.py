1f2b447856fec8b3ed16c6c0028d0a6227b0d412
Darshan Aswath <65482184+xanf-code@users.noreply.github.com>
2023-05-03 00:14:51 +0530

    Removed Stream

--- app/doors/door_status.py
+++ app/doors/door_status.py
@@ -4,7 +4,7 @@ import json
 DOOR_STATUS_ENDPOINT = 'https://doorhandler.netlify.app/.netlify/functions/api/device/a4b000cd76'
 
 def check_door_status():
-    r = requests.get(DOOR_STATUS_ENDPOINT, stream=True)
+    r = requests.get(DOOR_STATUS_ENDPOINT)
     if r.status_code == 200:
         return r.json()['status'] == 'open' 
     else:
